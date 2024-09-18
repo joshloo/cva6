357ae12ab2b609565983d0ca050076c7467165f2
Josh Loo <joshlootunglun@gmail.com>
2024-09-18 10:42:46 +0800

    Delete corev_apu/fpga/lattice/impl_1 directory

--- corev_apu/fpga/lattice/impl_1/synwork/cva6_1_impl_1_m_partition.tcl
+++
@@ -1,4 +0,0 @@
-set_partition_info -name "/cva6" -timestamp "1725942335"
-set_partition_info -name "/cva6/cva6" -timestamp "1725942335"
-set_partition_info -name "/cva6/cva6" -timestamp "1725942335"
-set_partition_info -name "/cva6/cva6" -timestamp "1725942335"
